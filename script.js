7c15655e080c27af6198f7156d1f40e672a423e4
Matt Pahuta <mhpahuta@gmail.com>
2024-08-07 11:02:59 -0700

    Revise conversion logic, add UI logic and updated styles

--- script.js
+++ script.js
@@ -1,69 +1,88 @@
-const numberInput = document.getElementById('number');
 const form = document.getElementById('form');
+const convertBtn = document.getElementById('convert-btn');
 const output = document.getElementById('output');
 
-function handleMessaging(value) {
-  // if button clicked with no value in numberInput
-  // - return 'Please enter a valid number'
-  // if numberInput contains a negative number
-  // - return 'Please enter a number greater than or equal to 1
-  // if numberInput contains a number >= 4000
-  // - return 'Please enter a number less than or equal to 3999
+function validateInput(strVal, intVal) {
   let errorMessage = '';
-  
-  if (value === 0) {
+  // check for error conditions
+  if (!strVal || strVal.match(/[e.]/g)) {
     errorMessage = 'Please enter a valid number';
-  } else if (value < 0) {
+  } else if (intVal < 1) {
     errorMessage = 'Please enter a number greater than or equal to 1';
-  } else if (value >= 4000) {
+  } else if (intVal > 3999) {
     errorMessage = 'Please enter a number less than or equal to 3999'
   } else {
-    return;
+    return true; // no error criteria met
   }
-  return errorMessage;
+  // add error msg to DOM, apply styling
+  output.textContent = errorMessage;
+  output.classList.add('warning');
+
+  return false; // an error has occurred
 }
 
+// Number to Roman Numeral conversion logic
 function convertToRoman(num) {
-  const reference = [
-    ['M', 1000],
-    ['CM', 900],
-    ['D', 500],
-    ['CD', 400],
-    ['C', 100],
-    ['XC', 90],
-    ['L', 50],
-    ['XL', 40],
-    ['X', 10],
-    ['IX', 9],
-    ['V', 5],
-    ['IV', 4],
-    ['I', 1]
-  ];
-  const result = [];
-
-  reference.forEach((arr) => {
-    while (num >= arr[1]) {
-      result.push(arr[0]);
-      num -= arr[1]
+  const romanNumRef = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  };
+  let romanNumResult = '';
+  
+  for (let key in romanNumRef) {
+    while (num >= romanNumRef[key]) {
+      romanNumResult += key;
+      num -= romanNumRef[key];
     }
-  })
-  return result.join('');
+  }
+  return romanNumResult;
 }
-
 
 function handleSubmit(e) {
   e.preventDefault();
-
-  const inputValue = Number(numberInput.value);
-  console.log('inputValue: ', inputValue)
-
-  const result = convertToRoman(inputValue);
-  output.innerText = result;
-
-  // output.textContent = handleMessaging(inputValue)
-  
-
+  updateUI();
 
 }
 
+function clearMessageOutput() {
+  output.textContent = '';
+  output.classList.remove('warning');
+}
+
+/* 
+ToDo:
+  - function to handle result/error element creation
+  - use createElement to create p element with error/result
+*/
+
+function updateUI() {
+  const numStr = document.getElementById('number').value;
+  const numInt = Number(numStr); // convert input val string to a number
+
+  output.classList.remove('visually-hidden');
+
+  clearMessageOutput();
+
+  if (validateInput(numStr, numInt)) {
+    output.textContent = convertToRoman(numInt);
+  }
+}
+
 form.addEventListener('submit', handleSubmit)
+
+// *** For FCC code editor only *** //
+convertBtn.addEventListener('click', () => {
+  console.log('clicked')
+  updateUI();
+});
